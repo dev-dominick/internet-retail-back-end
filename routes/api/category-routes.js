82b0d6fe4132c3c6dcac84c95a8b180a922c2398
Dom <Domalbano35@gmail.com>
2022-04-21 21:28:27 -0400

    set up category api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,19 +15,63 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
+    include: {
+      model: Product,
+      attributes: ['category_id']
+    }
+  }).then((data) => res.json(data))
+    .catch((err) => res.status(500).json(err))
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    if (!data) {
+      res.status(404).json({ message: 'No category found.'});
+      return;
+    }
+    res.json(data)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+    if (!data) {
+      res.status(404).json({ message: "No category found." });
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
